b1a77d005577009a582435bada85bf9018a451e5
zrihe <zrihend@gmail.com>
2020-08-21 13:26:07 +0300

    ex4.1

---
+++ ex4-1.js
@@ -0,0 +1,13 @@
+//Ex4.1 - Fibonacci - 
+
+
+const fibonacci =(n) => {
+    let previusNum1 = 1;
+    let previusNum2 = 1;
+    for(i = 2; i< n;i++){
+        previusNum1 >= previusNum2 ? previusNum2 += previusNum1 : previusNum1 += previusNum2;
+    }
+    return previusNum1 > previusNum2 ? previusNum1 : previusNum2;s
+}
+
+console.log(fibonacci(14));
