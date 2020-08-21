eea64515ff70fc8f18b50a42c14125b8f4c7680e
zrihe <zrihend@gmail.com>
2020-08-21 14:00:16 +0300

    ex5-3

---
+++ ex5-3.js
@@ -0,0 +1,17 @@
+
+
+
+function toCamelCase(str){
+    strArr = str.includes('-') ? str.split('-') : str.split('_')
+    for(const word of strArr){
+        let changeWord = word.replace(word[0],word[0].toUpperCase());
+        strArr[strArr.indexOf(word)] = changeWord;
+        
+    }
+    return strArr.join('')
+}
+
+
+console.log(toCamelCase("the-stealth-warrior"));  // returns "theStealthWarrior"
+
+console.log(toCamelCase("The_Stealth_Warrior")); // returns "TheStealthWarrior"
