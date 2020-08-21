db733bc5809c6b1afcd34a0d011372e490b63c06
zrihe <zrihend@gmail.com>
2020-08-21 11:24:42 +0300

    fix ex2

--- ex-2-2.js
+++ ex-2-2.js
@@ -1,21 +1,16 @@
 
 
-let binary = [0,1,0,0];
+let binary = [1,1,1,1];
 let binary2 = [1,0,1,0,1,0,0];
 
 
 function makeNumber(binary){
     let binaryReverse = binary.reverse();
     let binarySum = 0;
-    for(i = 1; i< binaryReverse.length-1;i++){
-        if(i === 2){
-            binarySum += (binaryReverse[i] *2);
-        }
-        else{
-            binarySum += (binaryReverse[i] * Math.pow(2, i));
-        }
+    for(i = 0; i< binaryReverse.length;i++){
+        i === 0 || i === 1 ? binarySum += (i+1)*binaryReverse[i] : binarySum += Math.pow(2,i)*binaryReverse[i];
     }
-    return binarySum
+    return binarySum;
 }
 
-console.log(makeNumber(binary2));
+console.log(makeNumber(binary));
