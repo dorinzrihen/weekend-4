72e4ef7b6cf6e944b89a529e9a975e669195f917
zrihe <zrihend@gmail.com>
2020-08-21 17:09:19 +0300

    ex6-3

---
+++ ex6-3.js
@@ -0,0 +1,21 @@
+
+
+function longest(a,b){
+    let arr = a.split('')+b.split('');
+    let obj = {};
+
+    for( const index of arr){
+        if((/[^,]/g).test(index)){
+            obj[index] = obj[index] ? obj[index]+=1 : obj[index] = 1;
+        }
+    }
+    arr = Object.keys(obj);
+    return arr.sort().join('');
+}
+
+
+
+
+a = "xyaabbbccccdefww";
+b = "xxxxyyyyabklmopq";
+console.log(longest(a, b));
